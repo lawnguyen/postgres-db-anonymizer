ef3f34f9549f16d0c3e1abb1189f323b1613f251
lawnguyen <law.lnguyen@gmail.com>
2024-02-06 00:59:36 -0800

    main function

---
+++ main.py
@@ -0,0 +1,34 @@
+import psycopg
+import yaml
+
+from repository import update_table
+
+
+def main():
+    # Read config file
+    with open("config.yaml", "r") as stream:
+        try:
+            config = yaml.safe_load(stream)
+
+            # Connect to an existing database
+            database_connection = config["database_connection"]
+            with psycopg.connect(
+                f"dbname={database_connection['database_name']} user={database_connection['user']}"
+            ) as conn:
+
+                # Open a cursor to perform database operations
+                with conn.cursor() as cur:
+                    for transformation in config["transformations"]:
+                        update_table(
+                            conn,
+                            cur,
+                            transformation["table"],
+                            transformation["columns"],
+                        )
+
+        except yaml.YAMLError as exc:
+            print(exc)
+
+
+if __name__ == "__main__":
+    main()
